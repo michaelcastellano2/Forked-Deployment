31170e412016287942a00749d55151e8689d6df8
Joshua Gorniak <joszek2004@gmail.com>
2025-03-12 11:39:16 -0400

    Fixed Auth

--- CollabRate/main/urls.py
+++ CollabRate/main/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
     path("", include("collabrate.urls")),
 ]
