2b6f45f60945c65c0c8acfd2c8f7bb318243b0f5
Landon <landonle2251@gmail.com>
2025-03-31 02:30:55 -0400

    Professor form creation, deletion, and editing

--- CollabRate/dashboard/urls.py
+++ CollabRate/dashboard/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
    path('courses/create/', views.create_course, name='create_course'),
    path('courses/leave/<str:join_code>/', views.leave_course, name='leave_course'),
    path('courses/delete/<str:join_code>/', views.delete_course, name='delete_course'),
-   path('courses/invite/<str:join_code>/<str:token>/', views.course_invite, name='course_invite')
+   path('courses/invite/<str:join_code>/<str:token>/', views.course_invite, name='course_invite'),
+   path('courses/<str:join_code>/create-form', views.create_form, name='create_form'),
+   path('courses/<str:join_code>/create-form-info/<int:form_id>', views.create_form_info, name='create_form_info'),
+   path('courses/<str:join_code>/create-form-questions/<int:form_id>', views.create_form_questions, name='create_form_questions'),
+   path('courses/<str:join_code>/view-forms/', views.view_forms, name='view_forms'),
+   path('courses/<str:join_code>/delete-form/<int:form_id>/', views.delete_form, name='delete_form'),
+   path('courses/<str:join_code>/edit-form/<int:form_id>/', views.edit_form, name='edit_form')
 ]
