fd8f96daff2afad58b06717d5f3cd439875941e2
Joshua Gorniak <joszek2004@gmail.com>
2025-04-15 13:02:04 -0400

    WOOHOO

--- CollabRate/course/urls.py
+++ CollabRate/course/urls.py
@@ -13,12 +13,7 @@ urlpatterns = [
     path('<str:join_code>/form/<int:course_form_id>/delete', views.delete_form, name='delete_form'),
 
     
-    # path('<str:join_code>/form/questions/<int:course_form_id>/', views.create_form_questions, name='create_form_questions'),
-
-    
-    # path('<str:join_code>/form/draft/<int:form_id>/', views.create_form_info, name='create_form_info'),
     path('<str:join_code>/form/view/', views.view_forms, name='view_forms'),
-    # path('<str:join_code>/form/delete/<int:form_id>/', views.delete_form, name='delete_form'),
     path('<str:join_code>/form/edit/<int:form_id>/', views.edit_form, name='edit_form'),
     path('<str:join_code>/', views.course_detail, name='course_detail'),
     path('course/<str:join_code>/clear/', views.clear_course_forms, name='clear_course_forms')
